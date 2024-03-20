d087ce37031213c80f0fde54383acb092a78cb56
Yuanyang Zhang <85085067+zhangyuanyang21@users.noreply.github.com>
2024-03-20 21:29:24 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -22,7 +22,7 @@ class Loss(nn.Module):
         mask = mask.bool()
         return mask
 
-    def forward_feature(self, h_i, h_j, S):
+    def CwCL(self, h_i, h_j, S):
         S_1 = S.repeat(2, 2)
 
         # S_1[S_1 > 0.9] = 1
@@ -44,23 +44,6 @@ class Loss(nn.Module):
         mask = self.mask_correlated_samples(N)
 
         negative_samples = sim1[mask].reshape(N, -1)
-
-        labels = torch.zeros(N).to(positive_samples.device).long()
-        logits = torch.cat((positive_samples, negative_samples), dim=1)
-        loss = self.criterion(logits, labels)
-        loss /= N
-        return loss
-    def forward_feature_original(self, h_i, h_j):
-        N = 2 * self.batch_size
-        h = torch.cat((h_i, h_j), dim=0)
-
-        sim = torch.matmul(h, h.T) / self.temperature_f
-        sim_i_j = torch.diag(sim, self.batch_size)
-        sim_j_i = torch.diag(sim, -self.batch_size)
-
-        positive_samples = torch.cat((sim_i_j, sim_j_i), dim=0).reshape(N, 1)
-        mask = self.mask_correlated_samples(N)
-        negative_samples = sim[mask].reshape(N, -1)
 
         labels = torch.zeros(N).to(positive_samples.device).long()
         logits = torch.cat((positive_samples, negative_samples), dim=1)
